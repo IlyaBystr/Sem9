31ab105640a49f2f5cfe25e32d1a9c9be6b0e4f4
Ilya <fox367213@mail.com>
2022-11-16 10:11:13 +0300

    Start Work and First Task

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N, 1));
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+
+    return (start + " " + PrintNumbers(start - 1, end));
+
+}
