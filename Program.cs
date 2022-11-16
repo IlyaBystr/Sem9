aafebe74628a27748facff3015ab2b8ecfcb0e5e
Ilya <fox367213@mail.com>
2022-11-16 11:30:35 +0300

    Third  Task

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ string PrintNumbers(int start, int end)
 /*
 Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от N до M.
 */
-
+/*
 Console.WriteLine("Введите число N");
 int N = int.Parse(Console.ReadLine()!);
 
@@ -35,4 +35,25 @@ int SumNumbers(int Sum, int NumbN, int NumbM)
 {
     if (NumbN == NumbM) return Sum;
     return SumNumbers(Sum = Sum + 1 + NumbN++, NumbN, NumbM);
+}
+*/
+
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+*/
+
+Console.WriteLine("Введите число M");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Функция Аккермана для чисел {M} и {N} = {Ackermann(M,N)}");
+
+int Ackermann(int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return Ackermann(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return Ackermann(numberM - 1, Ackermann(numberM, numberN - 1));
+return Ackermann(numberM, numberN);
 }
